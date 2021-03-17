269f0daf9627bd2975fa272405061c106d2cc17b
jacketsj <jacketsj@gmail.com>
2021-03-17 01:44:48 -0700

    Lazy build and run

---
+++ battleship/build_and_run_lazy.sh
@@ -0,0 +1,1 @@
+/opt/hipSYCL/bin/syclcc-clang --hipsycl-platform=rocm --hipsycl-gpu-arch=gfx802 precomputed_bitmask_unroll_custom_gpu.cpp -O2 --std=c++17 -o run_precomputed_bitmask_unroll_custom_gpu
