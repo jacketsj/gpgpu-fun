b42c38632b5974268ffdedaf66b01e380ea1aac6
jacketsj <jacketsj@gmail.com>
2021-03-20 21:13:42 -0700

    Change a commented line in build_and_run_lazy.sh

--- battleship/build_and_run_lazy.sh
+++ battleship/build_and_run_lazy.sh
@@ -1,3 +1,3 @@
 #/opt/hipSYCL/bin/syclcc-clang --hipsycl-platform=rocm --hipsycl-gpu-arch=gfx802 precomputed_bitmask_unroll_custom_gpu.cpp -O2 --std=c++17 -o run_precomputed_bitmask_unroll_custom_gpu
 /opt/hipSYCL/bin/syclcc-clang --hipsycl-platform=rocm --hipsycl-gpu-arch=gfx802 precomputed_bitmask_unroll_custom_gpu_parallelized.cpp -O2 --std=c++17 -o run_precomputed_bitmask_unroll_custom_gpu_parallelized
-#/opt/hipSYCL/bin/syclcc-clang --hipsycl-platform=cpu  precomputed_bitmask_unroll_custom_gpu.cpp -O2 --std=c++17 -o run_precomputed_bitmask_unroll_custom_gpu
+#/opt/hipSYCL/bin/syclcc-clang --hipsycl-platform=cpu  precomputed_bitmask_unroll_custom_gpu_parallelized.cpp -O2 --std=c++17 -o run_precomputed_bitmask_unroll_custom_gpu_parallelized
