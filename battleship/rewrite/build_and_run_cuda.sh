bcba96cef6ba6858f4350c951fb660e3d48d160e
jacketsj <jacketsj@gmail.com>
2021-07-28 16:17:35 -0500

    Add quick build script

---
+++ battleship/rewrite/build_and_run_cuda.sh
@@ -0,0 +1,6 @@
+/opt/hipSYCL/bin/syclcc-clang main_gpu.cpp --hipsycl-targets=cuda:sm_75 --cuda-path=/usr/local/cuda -O3 -o main_gpu
+echo "Compilation done, running..."
+time ./main_gpu
+# non-gpu version
+#g++ -O3 precomputed_bitmask_unroll_custom.cpp -o run_precomputed_bitmask_unroll_custom
+#time ./run_precomputed_bitmask_unroll_custom
